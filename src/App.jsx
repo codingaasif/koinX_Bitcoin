426e0822993bee8721e6ee6477ee6d531e57897a
Aasif Noor <aasifnoor98@gmail.com>
2025-01-21 17:11:23 +0530

    KoinX Bitcoin Website

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,7 @@
 import { useEffect } from 'react';
-// import PriceDisplay from './components/PriceDisplay';
-// import ChartComponent from './components/ChartComponent';
-// import TrendingCoins from './components/TrendingCoins';
-// import YouMayAlsoLike from './pages/YouMayAlsoLike';
-// import Navbar from './pages/Navbar';
-// import SentimentCard from './components/SentimentCard';
-// import AboutBitcoin from './components/AboutBitcoin';
-// import Tokenomics from './components/Tokenomics ';
-// import TeamSection from './components/TeamSection';
-// import CryptoCoinsDisplay from './components/CryptoCoinsDisplay';
 import { useDispatch } from 'react-redux';
 import { fetchBitcoinPrice, fetchTrendingCoins } from './redux-toolkit/slice';
-// import PerformancePage from './components/PerformancePage';
-// import createBrowserRouter from "react-router-dom"
+
 import KoinXRouter from './router/koinXRouter';
 import { RouterProvider } from 'react-router-dom';
 import { Box } from '@mui/material';
